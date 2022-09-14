6a732c43957348a1e7725e9dd2473076f70c74cb
kevinozkaraca <kevin.ozkaraca@gmail.com>
2022-09-14 20:20:23 +0300

    FOnction gyroscope

--- script.js
+++ script.js
@@ -63,22 +63,23 @@ acl.addEventListener("reading", () => {
   let gyroX = document.getElementById("gyroX");
   let gyroY = document.getElementById("gyroY");
   let gyroZ = document.getElementById("gyroZ");
-  gyroX.textContent = `X : ${acl.x}`;
-  gyroY.textContent = `Y : ${acl.y}`;
-  gyroZ.textContent = `Z : ${acl.y}`;
+  gyroX.textContent = `X : ${Math.round(acl.x)}`;
+  gyroY.textContent = `Y : ${Math.round(acl.y)}`;
+  gyroZ.textContent = `Z : ${Math.round(acl.z)}`;
   if (acl.x >= -2) {
     rightPressed = true;
     lastButtonPressed = "right";
-  } 
+  }
   if (acl.x >= 2) {
     leftPressed = true;
     lastButtonPressed = "left";
-  } 
-  if (Math.round(acl.x) == 0 || Math.round(acl.x) == 1 ){
+  }
+  if (Math.round(acl.x) == 0 || Math.round(acl.x) == 1) {
     leftPressed = false;
     rightPressed = false;
     upPressed = false;
     downPressed = false;
+  }
 });
 
 function GameObject() {
